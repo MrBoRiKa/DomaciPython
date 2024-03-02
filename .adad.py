74d6913e35c4221884ff5409145399ada4ea9201
MrBoRiKa <141438717+MrBoRiKa@users.noreply.github.com>
2024-03-02 17:48:15 +0000

    black1

--- .adad.py
+++ .adad.py
@@ -1,2 +1,11 @@
-a =[1, 2, 5, 7,-3]
-print(a[1:-1])
+num_input = input('Enter num:')
+num = int(num_input)
+
+first_dig = num // 100
+second_dig = (num % 100) //10
+third_dig = num % 10
+
+zbir_cifara = first_dig + second_dig + third_dig
+proizvod = first_dig * second_dig * third_dig
+
+print (proizvod-zbir_cifara)
